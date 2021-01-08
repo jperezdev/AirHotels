d1cd2ac6e883e778785153cf11d190e4f755c58b
James D. Pérez <39811810+jperezdev@users.noreply.github.com>
2021-01-07 23:13:07 -0400

    update flie

--- main.js
+++
@@ -1,9 +0,0 @@
-const targetBlankElement = document.querySelector(".test-link a");
-
-if(targetBlankElement.hasAttribute("target")){
-    console.log(targetBlankElement.getAttribute("target"));
-}
-
-else{
-    targetBlankElement.setAttribute("target","_blank");
-}
